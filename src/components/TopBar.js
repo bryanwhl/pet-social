db3f79717e8ff8a2508c68bd741f30e7ed1cd68e
Wang Zihan <e0523361@u.nus.edu>
2021-05-10 16:39:57 +0800

    Add side bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import {IconButton, AppBar, Toolbar, Grid, TextField} from '@material-ui/core';
-import React from 'react'
 import MenuIcon from '@material-ui/icons/Menu';
 import ChatIcon from '@material-ui/icons/Chat';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import SearchIcon from '@material-ui/icons/Search';
+import SideBar from './SideBar';
 
 const TopBar = () => {
+
+    const [drawerState, setDrawerState] = useState(false);
+
+    const openDrawer = () => {
+        console.log('Side menu opened');
+        setDrawerState(true);
+    }
+
+    const closeDrawer = () => {
+        console.log('Side menu closed');
+        setDrawerState(false);
+    }
 
     return (
         <div>
             <AppBar position="relative">
                 <Toolbar>
                     <Grid container spacing={1} alignItems="flex-end" justify="flex-start">
-                        <IconButton>
+                        <IconButton onClick={openDrawer}>
                             <MenuIcon />
                         </IconButton>
                     </Grid>
@@ -38,6 +51,7 @@ const TopBar = () => {
                     </Grid>
                 </Toolbar>
             </AppBar>
+            <SideBar position="relative" drawerState={drawerState} closeDrawer={closeDrawer}/>
         </div>
     )
 }
