843205ca3978b8a45a57f48a53690f4e15427d0b
Wang Zihan <e0523361@u.nus.edu>
2021-05-21 11:59:40 +0800

    Add Avatar and Name to profile popper

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import {IconButton, AppBar, Toolbar, Grid, TextField, Badge, Popper, Grow,
-    Paper, MenuList, ClickAwayListener, ListItem, ListItemIcon, ListItemText, Avatar} from '@material-ui/core';
+import { IconButton, AppBar, Toolbar, Grid, TextField, Badge, Popper, Grow,
+    Paper, MenuList, ClickAwayListener, ListItem, ListItemIcon, ListItemText, Avatar, Typography, Divider } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChatIcon from '@material-ui/icons/Chat';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
@@ -30,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
     },
     rightPopper: {
         zIndex: theme.zIndex.drawer + 1,
+        width: '200px',
     }
 }));
 
@@ -202,6 +203,15 @@ const TopBar = ({ logout, user, appState, setAppState }) => {
                                 <Paper>
                                     <ClickAwayListener onClickAway={handleProfileClose}>
                                         <MenuList autoFocusItem={profileOpen} id="profile-list-grow" onKeyDown={handleListKeyDown}>
+                                            <ListItem>
+                                                <ListItemIcon>
+                                                    <Avatar>
+                                                        {user.givenName[0]}
+                                                    </Avatar>
+                                                </ListItemIcon>
+                                                <Typography>{user.displayName}</Typography>
+                                            </ListItem>
+                                            <Divider />
                                             {profileItems.map(item => (
                                                 <ListItem
                                                     button
