32ae84d8ff86d234d8530817e348a631d6842ea3
bryanwhl <bryanwhl1999@gmail.com>
2021-05-23 22:20:00 +0800

    set up mock data in index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,42 @@ const {
 } = require('graphql')
 const app = express()
 
+const users = [
+  {
+    id: "1",
+    username: "admin",
+    password: "admin123",
+    accountType: "Personal",
+    givenName: "Ad",
+    familyName: "Min",
+    displayName: "Ad Min",
+    nameOrder: false,
+    avatarPath: './components/static/images/cute-dog.jpg'
+  },
+  {
+    id: "2",
+    username: "Pet Social",
+    password: "admin123",
+    accountType: "Business",
+    givenName: "Pet",
+    familyName: "Social",
+    displayName: "Pet Social",
+    nameOrder: false,
+    avatar: './components/static/images/cute-dog.jpg'
+  },
+  {
+    id: "3",
+    username: "bryanwhl",
+    password: "admin123",
+    accountType: "Personal",
+    givenName: "Bryan",
+    familyName: "Wong",
+    displayName: "Bryan Wong",
+    nameOrder: false,
+    avatar: './components/static/images/doctorstrange.jpg'
+  },
+]
+
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'HelloWorld',
